48cfaaf5ff130cfa594ebd5015dfb5a638884fdc
soonhong99 <67502252+soonhong99@users.noreply.github.com>
2024-01-27 19:09:05 +0900

    feat: 커서 변경, npc 상호작용 변경

--- Assets/Code/IntroNPC.cs
+++ Assets/Code/IntroNPC.cs
@@ -2,27 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroNPC : Collidable
 {
-    public string message;
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogue;
+    private int index;
 
-    private float cooldown = 4.0f;
-    private float lastShout;
+    public float wordSpeed;
+    public bool playerIsClose;
+    public GameObject contButton;
 
     protected override void Start()
     {
-        base.Start();
-        lastShout = - cooldown;
+        dialogueText.text = "";
+    }
+    protected override void Update()
+    {
+        // e키와 가까워졌을 때 말을 걸 수 있다.
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StopAllCoroutines();
+                StartCoroutine(Typing());
+            }
+        }
+
+        if(dialogueText.text == dialogue[index])
+        {
+            Debug.Log("can load button");
+            contButton.SetActive(true);
+        }
+
     }
 
-    protected override void OnCollide(Collider2D coll)
+    public void zeroText()
     {
-        if (Time.time - lastShout > cooldown)
-        {
-            lastShout = Time.time;
-            GameManager.instance.ShowText(message, 25, Color.white, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, 4.0f);
-        }
-        
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
     }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StopAllCoroutines();
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText() ;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Fighter"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Fighter"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+
 }
